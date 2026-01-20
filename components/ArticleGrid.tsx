a9cfbdcf53b43000dab99deccb762ee1a4d5ca56
LauhithN <LauhithN@users.noreply.github.com>
2026-01-19 19:53:46 -0500

    edit 4

--- components/ArticleGrid.tsx
+++ components/ArticleGrid.tsx
@@ -18,7 +18,7 @@ export function ArticleGrid({ articles }: ArticleGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       {articles.map((article) => (
         <ArticleCard key={article.id} article={article} />
       ))}
